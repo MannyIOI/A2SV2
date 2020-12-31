4e668abb11c6cc8270b6c8e191a7eca30fb28e31
Amanuel T <aman.teferi.80@gmail.com>
2020-12-31 18:34:29 +0300

    (feat) add negative for multiplication

--- Week-1/Day-1/big_multiply.py
+++ Week-1/Day-1/big_multiply.py
@@ -1,11 +1,22 @@
 from big_sum import bigSum
 
+from big_sum import bigSum
 # assumption num1 maybe greater in digits than num2 but num2 is not greater in digits than num1
 
 def big_multiply(num1: str, num2: str):
     carry = 0
     val = ''
     addables = []
+    positive = True
+
+    if num1[0] == '-':
+        num1 = num1[1: len(num1)]
+        positive = not positive
+
+    if num2[0] == '-':
+        num2 = num2[1: len(num2)]
+        positive = not positive
+
 
     # multiplication phase
     for i in range(len(num1) - 1, -1, -1):
@@ -14,26 +25,21 @@ def big_multiply(num1: str, num2: str):
             num2_int = int(num2[j])
 
             curr_val = (num1_int * num2_int) + carry
-            print(num1_int, num2_int, carry)
 
-            if curr_val > 9 and i > 0:
+            if curr_val > 9 and j > 0:
                 carry = curr_val // 10
                 curr_val = curr_val % 10
             
             val = str(curr_val) + val
 
-            if i == 0:
+            if j == 0:
+                addables.append(val)
                 carry = 0
                 val = ''
+    
+    val = bigSum(addables[1] + '0', '0'+addables[0])
+    if not positive: val = '-' + val
+    
+    return val
 
-
-        addables.append(val)
-
-    print(addables)
-
-    # # addition phase
-    # for i in addables:
-
-
-
-big_multiply('87', '92')
+print(big_multiply('87', '92'))
--- Week-1/Day-1/big_sum.py
+++ Week-1/Day-1/big_sum.py
@@ -1,22 +1,49 @@
 # assumption num1 maybe greater in digits than num2 but num2 is not greater in digits than num1
+# num1 has to be greater than num2
 
 def bigSum(num1: str, num2: str):
     carry = 0
     val = ''
+
+    num1:list = list(num1)
+    num2:list = list(num2)
+
+    both_positive = True
+
+    if num1[0] == '-':
+        num1 = num1[1: len(num1)]
+        both_positive = not both_positive
+
+    if num2[0] == '-':
+        num2 = num2[1: len(num2)]
+        both_positive = not both_positive
+    else:
+        if not both_positive:
+            return bigSum(''.join(num1), ''.join(num2))
+
     # go reverse
     for i in range(len(num1) - 1, -1, -1):
         num1_int = int(num1[i])
         num2_int = int(num2[i])
 
-        curr_val = num1_int + num2_int + carry
+        if both_positive: curr_val = num1_int + num2_int + carry
+        else:
+            if num1_int < num2_int:
+                num1_int = num1_int + 10
+                num1[i-1] = str(int(num1[i-1]) - 1)
+
+            curr_val = num1_int - num2_int
 
         if curr_val > 9 and i > 0:
-            curr_val = curr_val // 10
-            carry = curr_val % 10
+            carry = curr_val // 10
+            curr_val = curr_val % 10
         
+
         # use lists instead of concatenation for Big O optimization
-        val = str(curr_val) + val
+        val = str(int(str(curr_val) + val))
     
     return val
 
-print(bigSum('1', '9')) # 10
+# print(bigSum('1', '9')) # 10
+# print(bigSum('15', '25')) # 40
+# print(bigSum('-97','80')) # 5
