5e12eb017f3b34b3726d52a298a58c1d422bda98
Amanuel T <aman.teferi.80@gmail.com>
2021-01-05 19:04:01 +0300

    (fix) change sort colors algorithm

--- Week-2/Day-1/sort-colors.py
+++ Week-2/Day-1/sort-colors.py
@@ -2,11 +2,23 @@ def sortColors(nums: list) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] > nums[j]: nums[i], nums[j] = nums[j], nums[i]
+        i = 0
+        left = 0
+        right = len(nums) - 1
+        while i < right:
+            if nums[i] == 0:
+                nums[left], nums[i] = nums[i], nums[left]
+                left += 1
+                i += 1
+            elif nums[i] == 1:
+                i += 1
+            elif nums[i] == 2:
+                nums[right], nums[i] = nums[i], nums[right]
+                right -= 1
         
         return nums
+
+
 
 nums = [2,0,2,1,1,0]
 print(sortColors(nums))
